c1d0fc56ce8c73245d2004b9a98f9a0839f9d3e1
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-07-23 17:50:19 +0530

    Added api to signup user

--- server/index.js
+++ server/index.js
@@ -4,13 +4,17 @@ import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config();
 
+import User from "./models/User.js";
+import Transaction from "./models/Transaction.js";
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const connectDB = async ()=> {
+const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URl)
-    if(conn){
+    if (conn) {
         console.log(`MongoDB connected successfully`);
     }
 
@@ -18,13 +22,44 @@ const connectDB = async ()=> {
 connectDB();
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-        message : "Welcome to Expense Tracker"
+        message: "Welcome to Expense Tracker"
     })
 })
+
+app.post("/signup", async (req, res) => {
+    const { fullName, email, password, dob } = req.body;
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+        dob: new Date(dob)
+    });
+
+    try {
+        const savedUser = await user.save();
+
+        res.json({
+            success: true,
+            message: `Signup Successfully`,
+            data: savedUser
+        });
+    }
+    catch (e) {
+        res.json({
+            success: false,
+            message: e.message,
+            data: null
+        })
+    }
+
+})
+
+
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port  ${PORT}`);
 })
